1e001ff59d4f3cc314d7263fa37ef212fe5f1cd7
Daniele Finadri <33792173+DanieleF98@users.noreply.github.com>
2020-05-08 17:25:28 +0200

    Update and rename ex_02_02.py to ex_02_03.py

--- Esercizi Python/ex_02_03.py
+++ Esercizi Python/ex_02_03.py
@@ -1,3 +1,8 @@
+'''Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
+Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). 
+You should use input to read a string and float() to convert the string to a number. 
+Do not worry about error checking or bad user data.'''
+
 #gets hours and ratePerHour from keyboard input
 hours = input('How many hours : ')
 ratePerHour = input('What is the rate per hour?')
@@ -8,3 +13,9 @@ fratePerHour = float(ratePerHour)
 print('Pay :', (hours * ratePerHour)
 #we could have done this in only one step and convert the values directly into the print statement:
 #print('Pay :', (float(hours) * float(ratePerHour)))
+'''if we wanted to check the user data to exclude bad data, we could have used the try except
+try:
+    fhours = float(hours)
+except:
+    print('invalid data input')
+    quit() '''
