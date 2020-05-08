16bbf887974a77a56f78a8f304335398226ca90c
Daniele Finadri <33792173+DanieleF98@users.noreply.github.com>
2020-05-08 17:07:30 +0200

    Update ex_02_02.py
    added comments to the exercise

--- Esercizi Python/ex_02_02.py
+++ Esercizi Python/ex_02_02.py
@@ -1,3 +1,10 @@
+#gets hours and ratePerHour from keyboard input
 hours = input("How many hours : ")
 ratePerHour = input("What is the rate per hour?")
-print("Pay : ", (float(hours) * float(ratePerHour)))
+#converts hours and ratePerHour into float type so that we can use them as number
+fhours = float(hours)
+fratePerHour = float(ratePerHour)
+#prints the pay multiplying the value of hours and the ratePerHour
+print("Pay :", (hours * ratePerHour)
+#we could have done this in only one step and convert the values directly into the print statement:
+#print("Pay : ", (float(hours) * float(ratePerHour)))
